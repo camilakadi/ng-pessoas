0e6dda07fa47cd6da82ab248a0fed21de5919188
Camila Kadi <camilakadi@hotmail.com>
2025-07-07 19:15:54 -0300

    chore: remove pasta não utilizada e ajusta estilos no CSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { PessoaConsultaComponent } from './components/pessoa-consulta/pessoa-consulta.component';
-import { PessoaFormComponent } from './components/pessoa-form/pessoa-form.component';
 
 export const routes: Routes = [
   { path: '', component: PessoaConsultaComponent },
-  { path: 'cadastro', component: PessoaFormComponent },
   { path: '**', redirectTo: '' },
 ];
